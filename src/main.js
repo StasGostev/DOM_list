3e1991bade9002c2236306e485eda5f0ae7efe2d
StasGostev <spaceomen@gmail.com>
2019-05-22 17:42:49 +0300

    'New_Branch'

--- src/main.js
+++ src/main.js
@@ -1,21 +1,16 @@
 const main_container = document.getElementById('main_container');
 
 function createTree(container, data_obj) {
-
   const ul = document.createElement('ul');
-  const isEmpty = JSON.stringify(data_obj) === "{}"; 
-    
-  if (isEmpty) {
-    return;
-  }
+  const isEmptyObj = Object.keys(data_obj).length === 0;
 
   for (let key in data_obj) {
     const li = document.createElement('li');
     li.innerHTML = key;
-    ul.append(li);    
-    createTree(li, data_obj[key]);
+    ul.append(li);
+    createTree(li, data_obj[key]);       
   }
-  
-  container.append(ul)
+  container.append(ul);
 }
-createTree( main_container, data);
+
+createTree(main_container, data);
